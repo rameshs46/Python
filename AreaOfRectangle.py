05f4d7a4b554284dddd47c69df7491e7af9c92a2
Ramesh Sengamalai <rsengamalai@gmail.com>
2022-05-16 17:05:25 +0800

    Two programs

---
+++ AreaOfRectangle.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Program No: 2
+Created on Mon May 16 16:52:58 2022
+Description: Area of rectangle
+
+@author: rameshsengamalai
+"""
+l = int(input('Enter value for Length: '))
+b = int(input('Enter value for Breadth: '))
+area = l * b
+print(area)
+
---
+++ Product_TwoRealNumbers.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Program no: 1
+Created on Wed May 11 17:44:58 2022
+Description: Product of two real numbers
+
+@author: rameshsengamalai
+"""
+
+a = float(input ('Enter value for a = '))
+b = float(input ('Enter value for b = '))
+c = a * b
+print(c)
